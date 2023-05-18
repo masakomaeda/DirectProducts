36a0f4fe505162edccf90fe5d43d5d738e45db3a
masakomaeda <masakomaeda126@yahoo.co.jp>
2023-05-18 20:32:25 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Diagnostics;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var lists = new List<string>[]
+            {
+            new List<string> { "A", "B", "C" },
+            new List<string> { "x", "y", "Z" },
+            new List<string> { "a", "b", "c" },
+            new List<string> { "1", "2", "3" },
+                //new List<string> { "甲", "乙", "丙" },
+            };
+
+            
+            var ds = new List<string>();
+            ds = lists[0];
+            int i = 1;
+            do
+            {
+                ds = sub2(ds, lists[i]);
+                i++;
+            } while (i < lists.Length);
+
+
+            var count = 1;
+            ds.ForEach(s => Trace.WriteLine($"{count++:000}:{s}"));
+        }
+
+
+        static List<string> sub2(List<string> initList, List<string> list)
+        {
+            var ds = new List<string>();
+            initList.ForEach(lhs =>
+            {
+                list.ForEach(rhs => {
+                    ds.Add(lhs + rhs);
+                });
+            });
+
+
+            return ds;
+
+        }
+
+    }
+}
